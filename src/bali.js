59aa88b680c0a81671c857744cf7f4ecafbe0e84
xiaoyann <0x0886@gmail.com>
2016-07-29 15:48:54 +0800

    update

--- src/bali.js
+++ src/bali.js
@@ -38,10 +38,57 @@ export function union() {
   return items.filter((item, i) => items.indexOf(item) === i);
 }
 
+/**
+ * copy the values of all enumerable own properties 
+ * from one or more source objects to a target object. 
+ * @param  {any} target
+ * @return {object or array}
+ */
+export function assign(target) {
+  if (target == null) {
+    throw new TypeError('Cannot convert undefined or null to object');
+  }
+  target = Object(target);
+  for (let i = 1; i < arguments.length; i++) {
+    let source = arguments[i];
+    if (source != null) {
+      for (let k in source) {
+        target[k] = source[k];
+      }
+    }
+  }
+  return target;
+}
 
-// export function assign(target) {
-  
-// }
+/**
+ * recursively copy the values of all enumerable own properties 
+ * from one or more source objects to a target object. 
+ * @param  {any} target 
+ * @return {object or array}
+ */
+export function assignDeep(target) {
+  if (target == null) {
+    throw new TypeError('Cannot convert undefined or null to object');
+  }
+  target = Object(target);
+  for (let i = 1; i < arguments.length; i++) {
+    let source = arguments[i];
+    if (source != null) {
+      for (let k in source) {
+        let value = source[k];
+        let original = target[k];
+        if (isArray(value)) {
+          target[k] = assignDeep(isArray(original) ? original : [], value);
+        } else if (isObject(value)) {
+          target[k] = assignDeep(isObject(original) ? original : {}, value);
+        } else {
+          target[k] = value;
+        }
+      }
+    }
+  }
+  return target;
+}
 
 /**
  * creates a new object from the passed value by shallow clone
@@ -52,27 +99,27 @@ export function clone(obj) {
   if (isArray(obj)) {
     return obj.slice();  
   } else if (isObject(obj)) {
-    let res = {};
-    for (let k in obj) {
-      res[k] = obj[k];
-    }
-    return res;
+    return assign({}, obj);
   } else {
     return obj;
   }
 }
 
-// export function cloneDeep() {
+/**
+ * * creates a new object from the passed value by deep clone
+ * @param  {any} obj
+ * @return {any}
+ */
+export function cloneDeep(obj) {
+  if (isArray(obj)) {
+    return assignDeep([], obj);
+  } else if (isObject(obj)) {
+    return assignDeep({}, obj);
+  } else {
+    return obj;
+  }
+}
 
-// }
-
-// export function cloneWithout() {
-
-// }
-
-// export function cloneDeepWithout() {
-
-// }
 
 
 
--- test/test.js
+++ test/test.js
@@ -12,8 +12,10 @@ describe('isArray', function() {
   })
 
   it('should return false in other cases', function() {
-    assert.strictEqual(false, _.isArray(123))
     assert.strictEqual(false, _.isArray(false))
+    assert.strictEqual(false, _.isArray(null))
+    assert.strictEqual(false, _.isArray(undefined))
+    assert.strictEqual(false, _.isArray(123))
     assert.strictEqual(false, _.isArray({}))
     assert.strictEqual(false, _.isArray(function() {}))
   })
@@ -29,12 +31,15 @@ describe('isObject', function() {
     assert.strictEqual(false, _.isObject(null))
   })
 
-  it('should return false when obj is Function', function() {
-    assert.strictEqual(false, _.isObject(function() {}))
-  })
-
   it('should return false when obj is Array', function() {
     assert.strictEqual(false, _.isObject([]))
+  })
+
+  it('should return false in other cases', function() {
+    assert.strictEqual(false, _.isFunction(123))
+    assert.strictEqual(false, _.isFunction(false))
+    assert.strictEqual(false, _.isFunction(undefined))
+    assert.strictEqual(false, _.isObject(function() {}))
   })
 })
 
@@ -48,24 +53,98 @@ describe('isFunction', function() {
   it('should return false in other cases', function() {
     assert.strictEqual(false, _.isFunction([]))
     assert.strictEqual(false, _.isFunction({}))
+    assert.strictEqual(false, _.isFunction(123))
+    assert.strictEqual(false, _.isFunction(null))
     assert.strictEqual(false, _.isFunction(false))
+    assert.strictEqual(false, _.isFunction(undefined))
   })
 })
 
 
 describe('union', function() {
   it('should return an array of unique values', function() {
-    let result = _.union([1, 2], [3, 1, 2])
-    assert.isArray(result)
-    assert.lengthOf(result, 3)
-    assert.deepEqual([1, 2, 3], result)
-    assert.deepEqual([1, 2, false, {a: ''}], _.union([1, 2], 1, false, 1, {a: ''}))
+    let actual = _.union([1, 2], [4, 3, 1, 2])
+    assert.isArray(actual)
+    assert.lengthOf(actual, 4)
+    assert.deepEqual([1, 2, 4, 3], actual)
+  })
+
+  it('should work well with varied values', function() {
+    let expected = [1, 2, false, {a: ''}]
+    let actual = _.union([1, 2], 1, false, 1, {a: ''})
+    assert.deepEqual(expected, actual)
+  })
+})
+
+
+describe('assign', function() {
+  it('should throw a type error', function() {
+    assert.throws(_.assign, TypeError)
+  })
+
+  it('should return a combined Object', function() {
+    let actual = _.assign({a: 'a', b: 'b'}, {a: 1, c: 'c'})
+    assert.deepEqual({a: 1, b: 'b', c: 'c'}, actual)
+  })
+
+  it('should return a combined Array', function() {
+    assert.deepEqual([3, 4], _.assign([1, 2], [3, 4]))
+  })
+
+  it('should work well with varied values', function() {
+    let actual = _.assign({}, 1, false, null, [2], {a: 'a'})
+    assert.deepEqual({0: 2, a: 'a'}, actual)
+  })
+
+  it('primitives will be wrapped to objects', function() {
+    let expected = new Number(1)
+    expected.a = true
+    assert.deepEqual(expected, _.assign(1, {a: true}))
+    assert.deepEqual(new Boolean(false), _.assign(false))
+    assert.deepEqual(new String('aa'), _.assign('aa'))
+  })
+})
+
+
+describe('assignDeep', function() {
+  it('should throw a type error', function() {
+    assert.throws(_.assignDeep, TypeError)
+  })
+
+  it('should return a combined array', function() {
+    let source = [{a: 1} , 1, {a: [1]}]
+    let actual = _.assignDeep([], source, null, 1)
+    assert.isArray(actual)
+    assert.notStrictEqual(actual[0], source[0])
+    assert.notStrictEqual(actual[2].a, source[2].a)
+  })
+
+  it('should return a combined object', function() {
+    let source    = {a: [{a: 1}]}
+    let expected  = {a: [{a: 1, b: 1}]}
+    let actual = _.assignDeep({a: [{}]}, {a: [{a: 2, b: 1}]}, source)
+    assert.isObject(actual)
+    assert.deepEqual(expected, actual)
+    assert.notStrictEqual(actual.a, source.a)
+  })
+
+  it('should work well with varied values', function() {
+    let actual = _.assignDeep({}, 1, false, null, [2], {a: 'a'})
+    assert.deepEqual({0: 2, a: 'a'}, actual)
+  })
+
+  it('primitives will be wrapped to objects', function() {
+    let expected = new Number(1)
+    expected.a = true
+    assert.deepEqual(expected, _.assignDeep(1, {a: true}))
+    assert.deepEqual(new Boolean(false), _.assignDeep(false))
+    assert.deepEqual(new String('aa'), _.assignDeep('aa'))
   })
 })
 
 
 describe('clone', function() {
-  it('should return the passed value directly when it is not an Array or Object', function() {
+  it('Should return the passed value directly when it is neither an Object nor an Array', function() {
     assert.strictEqual(1, _.clone(1))
     assert.strictEqual(null, _.clone(null))
     assert.strictEqual(false, _.clone(false))
@@ -73,20 +152,70 @@ describe('clone', function() {
     assert.strictEqual(func, _.clone(func))
   })
 
-  it('should return a duplicate of the passed value', function() {
-    let arr = [1, 2]
-    assert.notStrictEqual(arr, _.clone(arr))
-    let obj = {a: 1}
-    assert.notStrictEqual(false, _.clone(obj))
+  it('Should work with Array as well as Object', function() {
+    let original = [1, 2]
+    let actual = _.clone(original)
+    assert.isArray(actual)
+    assert.deepEqual(original, original)
+    assert.notStrictEqual(actual, original)
+  })
+
+  it('The actual should not be equal the original value', function() {
+    let original = { a: [{ a: 1 }] }
+    let actual = _.clone(original)
+    assert.notStrictEqual(actual, original)
+  })
+
+  it('The child of actual and should be equal each other', function() {
+    let original = { a: [{ a: 1 }] }
+    let actual = _.clone(original)
+    assert.strictEqual(actual.a, original.a)
+  })
+
+  it('The structure of actual should be same as the original value', function() {
+    let original = { a: [{ a: 1 }] }
+    let actual = _.clone(original)
+    assert.deepEqual(actual, original)
   })
 })
 
 
-// describe('Learn Chai', function() {
-//   it('just test', function() {
-//     assert.typeOf([], 'array')
-//   })
-// })
+describe('cloneDeep', function() {
+  it('Should return the passed value directly when it is neither an Object nor an Array', function() {
+    assert.strictEqual(1, _.cloneDeep(1))
+    assert.strictEqual(null, _.cloneDeep(null))
+    assert.strictEqual(false, _.cloneDeep(false))
+    let func = function() {}
+    assert.strictEqual(func, _.cloneDeep(func))
+  })
+
+  it('Should work with Array as well as Object', function() {
+    let original = [1, {a: 1}]
+    let actual = _.cloneDeep(original)
+    assert.isArray(actual)
+    assert.deepEqual(original, original)
+    assert.notStrictEqual(actual, original)
+    assert.notStrictEqual(actual[1], original[1])
+  })
+
+  it('The actual should not be equal the original value', function() {
+    let original = { a: [{ a: 1 }] }
+    let actual = _.clone(original)
+    assert.notStrictEqual(actual, original)
+  })
+
+  it('The child of actual and should not be equal each other', function() {
+    let original = { a: [{ a: 1 }] }
+    let actual = _.cloneDeep(original)
+    assert.notStrictEqual(actual.a, original.a)
+  })
+
+  it('The structure of actual should be same as the original value', function() {
+    let original = { a: [{ a: 1 }] }
+    let actual = _.cloneDeep(original)
+    assert.deepEqual(actual, original)
+  })
+})
 
 
 
